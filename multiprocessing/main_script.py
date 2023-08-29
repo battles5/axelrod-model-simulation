49be4fb6939ec04258a96627d03a3139a63cf408
Orso Peruzzi <peruzzi.orso@gmail.com>
2023-08-29 10:12:44 +0200

    changed parameters for a new ran with L = 50 and F = 10. added plotly graph to show results.

--- multiprocessing/main_script.py
+++ multiprocessing/main_script.py
@@ -2,21 +2,30 @@ import numpy as np
 from multiprocessing import Pool
 from tqdm import tqdm
 from simulation_module import simulate
+import plotly.graph_objects as go
 
-L = 100
+L = 50
 F = 10
-steps = 100000000
+steps = 60000000
 
 def simulation_wrapper(q):
-    global L, F, steps
     return simulate(L, F, q, steps)
 
 if __name__ == "__main__":
-    q_values = np.linspace(10, 400, 40, dtype=int)
+    q_values = np.linspace(10, 401, 20, dtype=int)
 
     # Usando solo 6 core
     with Pool(processes=6) as pool:
         results = list(tqdm(pool.imap(simulation_wrapper, q_values), total=len(q_values)))
 
-    for q, result in zip(q_values, results):
-        print(f"q = {q}, Smax/L = {result}")
+    # Creazione del grafico
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(x=q_values, y=results, mode='lines+markers', name='Smax/L vs q'))
+
+    fig.update_layout(title='Smax/L vs q',
+                      xaxis=dict(title='q'),
+                      yaxis=dict(title='Smax/L'),
+                      )
+
+    fig.show()
