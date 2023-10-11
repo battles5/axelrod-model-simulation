19232000e9f6079bc79b93ed537b2d6d50d33d52
Orso Peruzzi <peruzzi.orso@gmail.com>
2023-10-11 12:51:20 +0200

    now main script produces well-labeled graphs and makes num_cycles runs each q in order to get an average of smax/n values, then collect it in a csv. added the variable in config.txt.

--- multiprocessing/main_script.py
+++ multiprocessing/main_script.py
@@ -3,14 +3,15 @@ from multiprocessing import Pool
 from tqdm import tqdm
 from simulation_module import simulate
 import plotly.graph_objects as go
+import csv
+import os
 
-# Funzione per leggere i valori dal file di configurazione
 def read_config(filename="config.txt"):
     with open(filename, 'r') as f:
         lines = f.readlines()
     config = {}
     for line in lines:
-        key, value = line.strip().split('=')
+        key, value = line.split('=')
         config[key.strip()] = eval(value.strip())
     return config
 
@@ -20,14 +21,14 @@ L = config["L"]
 F = config["F"]
 steps = config["steps"]
 q_values = np.linspace(config["q_start"], config["q_end"], config["q_step"], dtype=int)
+num_cycles = config["num_cycles"]
 
 def simulation_wrapper(q):
-    return simulate(L, F, q, steps)
-
-import csv
+    smax_values = [simulate(L, F, q, steps) for _ in range(num_cycles)]
+    return np.mean(smax_values)
 
 if __name__ == "__main__":
-    # Usando solo 6 core
+    # Using only 6 cores
     with Pool(processes=6) as pool:
         results = list(tqdm(pool.imap(simulation_wrapper, q_values), total=len(q_values)))
 
@@ -37,14 +38,14 @@ if __name__ == "__main__":
     fig.add_trace(go.Scatter(x=q_values,
                              y=results,
                              mode='lines+markers',
-                             name='Smax/L vs q',
+                             name='<Smax>/N vs q',
                              line=dict(color='royalblue', width=2),
                              marker=dict(size=8, color='rgba(255, 182, 193, .9)', line=dict(color='rgba(152, 0, 0, .8)', width=2))
                              ))
 
     fig.update_layout(
         title={
-            'text': 'Smax/L vs q',
+            'text': '<Smax>/N vs q',
             'y':0.9,
             'x':0.5,
             'xanchor': 'center',
@@ -52,7 +53,7 @@ if __name__ == "__main__":
             'font': dict(size=24)
         },
         xaxis=dict(title='q', titlefont_size=16, tickfont_size=14, gridcolor='gray'),
-        yaxis=dict(title='Smax/L', titlefont_size=16, tickfont_size=14, gridcolor='gray'),
+        yaxis=dict(title='<Smax>/N', titlefont_size=16, tickfont_size=14, gridcolor='gray'),
         plot_bgcolor='rgba(230, 230, 230, 0.8)',  # sfondo chiaro
         width=900,
         height=600,
@@ -60,9 +61,13 @@ if __name__ == "__main__":
 
     fig.show()
 
+    # Ensure the 'graph' directory exists
+    if not os.path.exists('graph'):
+        os.makedirs('graph')
+
     # Esporta i dati in CSV
-    with open('graph_data.csv', 'w', newline='') as csvfile:
+    with open('graph/graph_data.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["q", "Smax/L"])
+        writer.writerow(["q", "<Smax>/N"])
         for q, result in zip(q_values, results):
             writer.writerow([q, result])
